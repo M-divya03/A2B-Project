7cc4f9ced01718e9cf375e1f44652572f4d0e780
M-divya03 <divyamohan3602@gmail.com>
2024-04-09 12:58:45 +0530

    repo commit

--- src/main/java/com/example/MenuDisplay/controller/DisplayController.java
+++ src/main/java/com/example/MenuDisplay/controller/DisplayController.java
@@ -6,6 +6,7 @@ import com.example.MenuDisplay.service.CategoryService;
 import com.example.MenuDisplay.service.ItemsService;
 import com.example.MenuDisplay.serviceImpl.CategoryServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/home")
+@CrossOrigin("*")
 public class DisplayController {
 
 
